fb9ba913ba9562c44b683bbc2b2ccea2798b1503
Matt Munch <mattmunch94@gmail.com>
2020-01-06 14:11:33 -0800

    reviewer routes complete and tested

--- __tests__/reviewers.test.js
+++ __tests__/reviewers.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Reviewer = require('../lib/models/Reviewer');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -13,8 +14,40 @@ describe('app routes', () => {
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
+  let reviewer;
+  beforeEach(async() => {
+    reviewer = await Reviewer.create({
+      name: 'Turd Burgular',
+      company:'Roto-Rooter'
+    });
+  });
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('has a get all reviewers route', () => {
+    return request(app)
+      .get('/api/v1/reviewers')
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: reviewer.id,
+          name: 'Turd Burgular',
+          company:'Roto-Rooter',
+          __v:0
+        }]);
+      });
+  });
+  it('has a get reviewer by id route', () => {
+    return request(app)
+      .get(`/api/v1/reviewers/${reviewer.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: reviewer.id,
+          name: 'Turd Burgular',
+          company:'Roto-Rooter',
+          __v:0
+        });
+      });
   });
 });
