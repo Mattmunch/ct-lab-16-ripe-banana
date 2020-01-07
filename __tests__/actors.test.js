149346d4a161c25231bfc89ca0a2d2b95e8dc22a
Matt Munch <mattmunch94@gmail.com>
2020-01-06 17:49:33 -0800

    close on the actors routes

--- __tests__/actors.test.js
+++ __tests__/actors.test.js
@@ -5,6 +5,8 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Actor = require('../lib/models/Actor');
+const Film = require('../lib/models/Film');
+const Studio = require('../lib/models/Studio');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -15,12 +17,31 @@ describe('app routes', () => {
     return mongoose.connection.dropDatabase();
   });
   let actor;
+  let films;
+  let studio;
   beforeEach(async() => {
+    studio = await Studio.create({
+      name: 'Paramount Pictures',
+      address: {
+        city: 'portland',
+        state: 'oregon',
+        country: 'USA'
+      },
+    });
     actor = await Actor.create({
       name: 'Seth Rogen',
       dob: '1991-12-23',
-      pob:'LA'
+      pob: 'LA'
     });
+    films = await Film.create([{
+      title: 'AirBud',
+      studio: studio._id,
+      released: 2020,
+      cast: [{
+        role: 'poopoo',
+        actor: actor._id
+      }]
+    }]);
   });
 
   afterAll(() => {
@@ -34,23 +55,19 @@ describe('app routes', () => {
       .then(res => {
         expect(res.body).toEqual([{
           _id: actor.id,
-          name: 'Seth Rogen',
-          dob: '1991-12-23T00:00:00.000Z',
-          pob:'LA',
-          __v:0
+          name: 'Seth Rogen'
         }]);
       });
   });
   it('has a get actor by id route', () => {
     return request(app)
-      .get(`/api/v1/actors/${actor.id}`)
+      .get(`/api/v1/actors/${actor._id}`)
       .then(res => {
         expect(res.body).toEqual({
-          _id: actor.id,
           name: 'Seth Rogen',
           dob: '1991-12-23T00:00:00.000Z',
           pob:'LA',
-          __v:0
+          films: films.map(film => ({ _id: actor._id.toString(), title: film.title, studio: film.released }))
         });
       });
   });
